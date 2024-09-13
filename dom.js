fee4d347712d1ceefb7c7ed9f77dcda4dfc9f3ae
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-09-13 11:47:01 +0600

    provide styles with JS

--- dom.js
+++ dom.js
@@ -1,2 +1,13 @@
 console.log('hello from JS');
 console.log(document);
+
+
+// <!-- 24.4 -->
+
+// <!-- document.querySelectorAll('fruits-title li') -->
+//  <!--  querySelectorAll -->
+   const someLi = document.querySelectorAll('.fruits-container li');
+//    console.log(someLi);
+   for(const li of someLi){
+    console.log(li.innerText);
+   }
