b645ed67276310a992cd32f6037dd03874bdd466
salehiin <salehin.ay@gmail.com>
2024-09-14 22:29:05 +0600

    class add remove and all sort of CSS manipulation can be done by JS ie; CSS in JS

--- dom.js
+++ dom.js
@@ -6,8 +6,30 @@ console.log(document);
 
 // <!-- document.querySelectorAll('fruits-title li') -->
 //  <!--  querySelectorAll -->
-   const someLi = document.querySelectorAll('.fruits-container li');
+   // const someLi = document.querySelectorAll('.fruits-container li');
 //    console.log(someLi);
-   for(const li of someLi){
-    console.log(li.innerText);
-   }
+   // for(const li of someLi){
+   //  console.log(li.innerText);
+   // }
+
+   // const title = document.getElementById('fruits-title')
+   // title.getAttribute('id')
+   // title.getAttribute('class')
+   // title.classList
+   // title.classList.add('moreClass')
+   // title.classList.remove('oneMore')
+   // title.setAttribute('title', 'tooltip set by Javascript')
+
+   // document.getElementsByClassName('testClass')
+
+   // document.getElementsByClassName('testClass')[0].innerHTML
+   // document.getElementsByClassName('testClass')[0].innerHTML = '<h2>ABCD</h2>'
+   // document.getElementsByClassName('testClass')[0].innerText
+
+   // const title = document.getElementById('anyId');
+   // const text = title.innerText;
+   // console.log(text);
+   // const html = title.innerHTML;
+   // console.log(html);
+
+   // 24.6
---
+++ styleInJs.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('section');
+// console.log(sections);
+for(const section of sections){
+    // console.log(section);
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.textAlign = 'center';
+    section.style.borderRadius = '5px';
+    section.style.backgroundColor = 'lightgray';
+}
+
+// const placesContainer = document.getElementById('places-container');
+// placesContainer.style.backgroundColor = 'yellow'
+
+const placesContainer = document.getElementById('places-container')
+placesContainer.classList.add('yellow-bg');
+placesContainer.classList.remove('large-text')
+
+// 24.7
